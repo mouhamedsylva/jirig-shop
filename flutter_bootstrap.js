da3f9fa74844a6c387c13ed5d6e71b1d94198ac7
Mouhamed Sylva <thicosylva@gmail.com>
2026-03-25 16:23:22 +0000

    Retours implémentés

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2780676223" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "795351098" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
