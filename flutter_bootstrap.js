ee03ebbc38f857535efa8aa93d1917776647ddef
Mouhamed Sylva <thicosylva@gmail.com>
2026-04-16 12:29:36 +0000

    Nouveau build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2903675633" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4161392013" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
