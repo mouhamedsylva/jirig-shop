b9cbfa0ec759c76de29920a608817be3dc1d0e76
Mouhamed Sylva <thicosylva@gmail.com>
2026-04-21 13:10:49 +0000

    Update service worker version and adjust promotional discount to -60%

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1612392650" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1442812928" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
